64c7ead4dcdfcb062910487767803044bb6e38d9
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-28 00:52:04 +0800

    Refactor code to use AnswerButton component for displaying answer options

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:quiz_app/answer_button.dart";
+import "package:quiz_app/data/questions.dart";
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,8 +12,38 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    final currentQuestion = questions[currentQuestionIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnwers().map((answer) {
+              return AnswerButton(answerText: answer, onTap: answerQuestion);
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
