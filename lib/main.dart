d83b78a3ac3d54202b1218bed8277c80abc9f792
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-27 21:42:15 +0800

    Refactor code to use Quiz class instead of StartScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomLeft,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
-  }
+  runApp(const Quiz());
 }
